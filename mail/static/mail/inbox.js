35bb45350b201b50d5d524f34fffe00008e2670e
Ryan Barnes <rybarnes@gmail.com>
2021-07-14 21:23:11 -0400

    first

---
+++ mail/static/mail/inbox.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  // Use buttons to toggle between views
+  document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+  document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#submit').addEventListener('click', send_email);
+
+  // By default, load the inbox
+  load_mailbox('inbox');
+});
+
+function send_email(event){
+  event.preventDefault();
+  console.log(document.querySelector('#compose-recipients').value)
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+      load_mailbox('inbox')
+  });
+  
+}
+
+function compose_email() {
+  
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#submit').addEventListener('click', send_email);
+
+}
+
+function load_mailbox(mailbox) {
+  
+  // Show the mailbox and hide other views
+  document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  // Show the mailbox name
+  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+    // Print emails
+    emails.forEach(email => {
+      console.log(email)
+      const element = document.createElement('div');
+      element.innerHTML = email.sender + "  :  " + email.subject + "  |   " + email.timestamp
+      document.querySelector('#emails-view').append(element)
+    })
+
+    // ... do something else with emails ...
+});
+
+}
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Project 3: Mail
+## For CS50W
+
+### TODO
+* Send mail
+    - Take form inputs to send email
+    - redirect to inbox
+* Mailbox
+    - Get all emails for the mailbox
+    - Display each email in its own box
+    - White background for unread, grey background for read emails.
+* View Email
+    - Get the specific email and content.
+    - Likely needs its own div to display in index.html
+    - Use PUT request to set email as read.
+* Archive and Unarchive
+    - In view email, user should be able to archive an email, or to unarchive
+    - Use put request to change status
+    - Redirect to inbox after updated archive status
+* Reply
+    - When viewing an email, user can hit reply
+    - Redirect to compose, with form fields pre-populated.
+    - Append RE: to subject, and original body.
+
+### Work Log
+
+#### 7.14.2021
+* Email submission form works. Redirects, pretty much completed.
+* Emails are geting displayed, if a bit ugly at the moment.
